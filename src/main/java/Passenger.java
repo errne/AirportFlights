f39525e00e02da0dc3bc775732b93c6358de7bad
Ernest Fraiman <upapaus@gmail.com>
2019-01-12 17:08:18 +0000

    passenger class created

---
+++ src/main/java/Passenger.java
@@ -0,0 +1,11 @@
+public class Passenger {
+    String name;
+
+    public Passenger(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/test/java/PassengerTest.java
@@ -0,0 +1,16 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PassengerTest {
+
+    @Before
+    public void before() {
+        Passenger john = new Passenger("John");
+    }
+
+    @Test
+    public void getName() {
+    }
+}
